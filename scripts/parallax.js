383184b926bb9d6cc85ce27021a142c1ccfed7b7
Fynthesizer <finn.mitchellanyon@gmail.com>
2021-12-21 15:27:11 +1300

    added parallax background effect

---
+++ scripts/parallax.js
@@ -0,0 +1,10 @@
+var speed = 0.15;
+var bg = document.querySelector("body");
+
+document.onload = setBgPos();
+window.addEventListener("scroll", setBgPos);
+
+function setBgPos(){
+    var distance = window.scrollY;
+    bg.style.backgroundPositionY = (distance * speed * -1) + "px";
+}
