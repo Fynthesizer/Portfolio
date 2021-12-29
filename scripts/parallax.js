7e505663b79eff961d27c7adf3c5ce7eb619cbd5
Fynthesizer <finn.mitchellanyon@gmail.com>
2021-12-29 15:43:24 +1300

    Added link icons

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -4,7 +4,7 @@ var bg = document.querySelector("body");
 document.onload = setBgPos();
 window.addEventListener("scroll", setBgPos);
 
-function setBgPos(){
+function setBgPos() {
     var distance = window.scrollY;
     bg.style.backgroundPositionY = (distance * speed * -1) + "px";
 }
