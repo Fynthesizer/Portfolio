20fdd2b24e87993e6ecdabcc3cea4dd30236c9ad
Fynthesizer <finn.mitchellanyon@gmail.com>
2022-05-03 17:45:00 +1200

    added new track

--- scripts/musicplayer.js
+++ scripts/musicplayer.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
     startplayer();
-}, false);
+  },
+  false
+);
 var currentTrack = 0;
 var player;
 var tracklist;
@@ -9,257 +13,271 @@ var title;
 var canvas, ctx, audioCtx, analyser, source, data;
 
 var tracks = [
-    {
-        "title": "The Enchanted Forest",
-        "audio": "/audio/enchantedforest.mp3",
-        "art": null,
-        "genre": "Orchestral"
-    },
-    {
-        "title": "Equine - Main Theme",
-        "audio": "/audio/equine.mp3",
-        "art": "/images/equine.png",
-        "genre": "Orchestral"
-    },
-    {
-        "title": "Crime of Slashin'",
-        "audio": "/audio/crimeofslashin.mp3",
-        "art": null,
-        "genre": "Darksynth"
-    },
-    {
-        "title": "Angry Peaches",
-        "audio": "/audio/angrypeaches.mp3",
-        "art": "/images/angrypeaches.png",
-        "genre": "Chiptune-Metal"
-    },
-    {
-        "title": "Trigger Witch - The Good, the Bad",
-        "audio": "/audio/thegoodthebad.mp3",
-        "art": "/images/triggerwitch.png",
-        "genre": "16-Bit Western"
-    },
-    {
-        "title": "Trigger Witch - The Ugly",
-        "audio": "/audio/theugly.mp3",
-        "art": "/images/triggerwitch.png",
-        "genre": "Western Metal"
-    },
-    {
-        "title": "Showdown",
-        "audio": "/audio/showdown.mp3",
-        "art": null,
-        "genre": "Synthwave"
-    },
-    {
-        "title": "The Sleepy Swamp",
-        "audio": "/audio/grottygrotto.mp3",
-        "art": null,
-        "genre": "Orchestral"
-    },
-    {
-        "title": "Space Pingaz",
-        "audio": "/audio/spaceorgy.mp3",
-        "art": null,
-        "genre": "Funky House"
-    },
-    {
-        "title": "Stranded - Figment",
-        "audio": "/audio/figment.mp3",
-        "art": "/images/stranded.png",
-        "genre": "Ambient"
-    },
-    {
-        "title": "Equine - Main Menu",
-        "audio": "/audio/equinemenu.mp3",
-        "art": "/images/equine.png",
-        "genre": "Orchestral"
-    },
-    {
-        "title": "Reoriention Week - RE4",
-        "audio": "/audio/reo.mp3",
-        "art": "/images/reorientationweek.png",
-        "genre": "Synthwave"
-    },
-    {
-        "title": "Ancient Mystery",
-        "audio": "/audio/ancientmystery.mp3",
-        "art": null,
-        "genre": "Hybrid Orchestral"
-    },
-    {
-        "title": "Equine - Anara",
-        "audio": "/audio/anara.mp3",
-        "art": "/images/equine.png",
-        "genre": "Orchestral"
-    },
-    {
-        "title": "Trigger Witch - Poltergeist Prince",
-        "audio": "/audio/poltergeistprince.mp3",
-        "art": "/images/triggerwitch.png",
-        "genre": "Synth Metal"
-    }
-]
+  {
+    title: "The Beginning of a Grand Adventure",
+    audio: "/audio/grandadventure.mp3",
+    art: "/images/magicalfantasyforest.jpg",
+    genre: "Orchestral",
+  },
+  {
+    title: "Enchanted Forest",
+    audio: "/audio/enchantedforest.mp3",
+    art: "/images/magicalfantasyforest.jpg",
+    genre: "Orchestral",
+  },
+  {
+    title: "Equine - Main Theme",
+    audio: "/audio/equine.mp3",
+    art: "/images/equine.png",
+    genre: "Orchestral",
+  },
+  {
+    title: "Crime of Slashin'",
+    audio: "/audio/crimeofslashin.mp3",
+    art: null,
+    genre: "Darksynth",
+  },
+  {
+    title: "Angry Peaches",
+    audio: "/audio/angrypeaches.mp3",
+    art: "/images/angrypeaches.png",
+    genre: "Chiptune-Metal",
+  },
+  {
+    title: "Trigger Witch - The Good, the Bad",
+    audio: "/audio/thegoodthebad.mp3",
+    art: "/images/triggerwitch.png",
+    genre: "16-Bit Western",
+  },
+  {
+    title: "Trigger Witch - The Ugly",
+    audio: "/audio/theugly.mp3",
+    art: "/images/triggerwitch.png",
+    genre: "Western Metal",
+  },
+  {
+    title: "Showdown",
+    audio: "/audio/showdown.mp3",
+    art: null,
+    genre: "Synthwave",
+  },
+  {
+    title: "The Sleepy Swamp",
+    audio: "/audio/grottygrotto.mp3",
+    art: "/images/magicalfantasyforest.jpg",
+    genre: "Orchestral",
+  },
+  {
+    title: "Space Pingaz",
+    audio: "/audio/spaceorgy.mp3",
+    art: null,
+    genre: "Funky House",
+  },
+  {
+    title: "Stranded - Figment",
+    audio: "/audio/figment.mp3",
+    art: "/images/stranded.png",
+    genre: "Ambient",
+  },
+  {
+    title: "Equine - Main Menu",
+    audio: "/audio/equinemenu.mp3",
+    art: "/images/equine.png",
+    genre: "Orchestral",
+  },
+  {
+    title: "Reoriention Week - RE4",
+    audio: "/audio/reo.mp3",
+    art: "/images/reorientationweek.png",
+    genre: "Synthwave",
+  },
+  {
+    title: "Ancient Mystery",
+    audio: "/audio/ancientmystery.mp3",
+    art: null,
+    genre: "Hybrid Orchestral",
+  },
+  {
+    title: "Equine - Anara",
+    audio: "/audio/anara.mp3",
+    art: "/images/equine.png",
+    genre: "Orchestral",
+  },
+  {
+    title: "Trigger Witch - Poltergeist Prince",
+    audio: "/audio/poltergeistprince.mp3",
+    art: "/images/triggerwitch.png",
+    genre: "Synth Metal",
+  },
+];
 
 var defaultArt = "/images/self portrait_underlay.png";
 
 function startplayer() {
-    player = document.getElementById('mp-audio');
-    player.controls = false;
-    player.addEventListener("timeupdate", updateUI);
-    player.addEventListener("canplaythrough", updateUI);
-    player.addEventListener("canplaythrough", trackLoaded);
-    player.addEventListener("ended", next);
+  player = document.getElementById("mp-audio");
+  player.controls = false;
+  player.addEventListener("timeupdate", updateUI);
+  player.addEventListener("canplaythrough", updateUI);
+  player.addEventListener("canplaythrough", trackLoaded);
+  player.addEventListener("ended", next);
 }
 
 function play_aud() {
-    let btn = document.getElementById('mp-play');
-    if (player.paused) {
-        player.play();
-        btn.classList.replace('fa-play', 'fa-pause');
-    } else {
-        player.pause();
-        btn.classList.replace('fa-pause', 'fa-play');
-    }
+  let btn = document.getElementById("mp-play");
+  if (player.paused) {
+    player.play();
+    btn.classList.replace("fa-play", "fa-pause");
+  } else {
+    player.pause();
+    btn.classList.replace("fa-pause", "fa-play");
+  }
 }
 
 function pause_aud() {
-    player.pause();
+  player.pause();
 }
 
 function stop_aud() {
-    player.pause();
-    player.currentTime = 0;
+  player.pause();
+  player.currentTime = 0;
 }
 
 function next() {
-    if(currentTrack < tracks.length - 1) changeTrack(currentTrack+1);
-    else changeTrack(0);
-    player.play();
+  if (currentTrack < tracks.length - 1) changeTrack(currentTrack + 1);
+  else changeTrack(0);
+  player.play();
 }
 
 function previous() {
-    if(currentTrack > 0) changeTrack(currentTrack-1);
-    else changeTrack(tracks.length - 1);
-    player.play();
+  if (currentTrack > 0) changeTrack(currentTrack - 1);
+  else changeTrack(tracks.length - 1);
+  player.play();
 }
 
-
 function change_vol() {
-    player.volume = document.getElementById("mp-volslider").value;
+  player.volume = document.getElementById("mp-volslider").value;
 }
 
 function change_pos() {
-    player.currentTime = player.duration * document.getElementById("mp-seekbar").value;
+  player.currentTime =
+    player.duration * document.getElementById("mp-seekbar").value;
 }
 
 function updateUI() {
-    if(player.readyState > 1) document.getElementById("mp-seekbar").value = player.currentTime / player.duration;
-    document.getElementById("mp-currenttime").innerHTML = fmtMSS(Math.round(player.currentTime));
-    //document.getElementById("mp-length").innerHTML = fmtMSS(Math.round(player.duration));
-    let btn = document.getElementById('mp-play');
-    if (player.paused) btn.classList.replace('fa-pause', 'fa-play');
-    else btn.classList.replace('fa-play', 'fa-pause');
+  if (player.readyState > 1)
+    document.getElementById("mp-seekbar").value =
+      player.currentTime / player.duration;
+  document.getElementById("mp-currenttime").innerHTML = fmtMSS(
+    Math.round(player.currentTime)
+  );
+  //document.getElementById("mp-length").innerHTML = fmtMSS(Math.round(player.duration));
+  let btn = document.getElementById("mp-play");
+  if (player.paused) btn.classList.replace("fa-pause", "fa-play");
+  else btn.classList.replace("fa-play", "fa-pause");
 }
 
 function trackLoaded() {
-    document.getElementById("mp-seekbar").value = player.currentTime / player.duration;
-    document.getElementById("mp-currenttime").innerHTML = fmtMSS(Math.round(player.currentTime));
-    document.getElementById("mp-length").innerHTML = fmtMSS(Math.round(player.duration));
-    if (tracks[currentTrack].art != null) art.src = tracks[currentTrack].art;
-    else art.src = defaultArt;
-    title.innerHTML = tracks[currentTrack].title;
-    //player.currentTime = 0;
+  document.getElementById("mp-seekbar").value =
+    player.currentTime / player.duration;
+  document.getElementById("mp-currenttime").innerHTML = fmtMSS(
+    Math.round(player.currentTime)
+  );
+  document.getElementById("mp-length").innerHTML = fmtMSS(
+    Math.round(player.duration)
+  );
+  if (tracks[currentTrack].art != null) art.src = tracks[currentTrack].art;
+  else art.src = defaultArt;
+  title.innerHTML = tracks[currentTrack].title;
+  //player.currentTime = 0;
 }
 
 function populateTracklist() {
-    for (let i = 0; i < tracks.length; i++) {
-        var track = document.createElement('li');
-        track.innerHTML = tracks[i].title;
-        track.addEventListener("click", function () {
-            changeTrack(i);
-            player.play();
-        })
-        var trackGenre = document.createElement('span');
-        trackGenre.innerHTML = tracks[i].genre;
-        trackGenre.className = "track-genre";
-        track.appendChild(trackGenre);
+  for (let i = 0; i < tracks.length; i++) {
+    var track = document.createElement("li");
+    track.innerHTML = tracks[i].title;
+    track.addEventListener("click", function () {
+      changeTrack(i);
+      player.play();
+    });
+    var trackGenre = document.createElement("span");
+    trackGenre.innerHTML = tracks[i].genre;
+    trackGenre.className = "track-genre";
+    track.appendChild(trackGenre);
 
-        // Add it to the list:
-        tracklist.appendChild(track);
-    }
+    // Add it to the list:
+    tracklist.appendChild(track);
+  }
 }
 
 function changeTrack(trackNum) {
-    currentTrack = trackNum;
-    document.getElementById("mp-seekbar").value = 0;
-    player.src = tracks[trackNum].audio;
-    let tracklistItem = tracklist.children[trackNum];
-    var current = tracklist.getElementsByClassName("active");
-    if (current.length > 0) current[0].className = "";
-    tracklistItem.className += "active";
+  currentTrack = trackNum;
+  document.getElementById("mp-seekbar").value = 0;
+  player.src = tracks[trackNum].audio;
+  let tracklistItem = tracklist.children[trackNum];
+  var current = tracklist.getElementsByClassName("active");
+  if (current.length > 0) current[0].className = "";
+  tracklistItem.className += "active";
 }
 
 function fmtMSS(s) {
-    return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s
-};
+  return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
+}
 
 $(document).ready(function () {
-    tracklist = document.getElementById('mp-tracklist');
-    art = document.getElementById('mp-art');
-    title = document.getElementById('mp-title');
+  tracklist = document.getElementById("mp-tracklist");
+  art = document.getElementById("mp-art");
+  title = document.getElementById("mp-title");
 
-    canvas = document.getElementById('mp-visualiser');
-    if (canvas != undefined) {
-        ctx = canvas.getContext("2d");
-        ctx.canvas.width = canvas.width;
-        ctx.canvas.height = canvas.height;
-        audioCtx = new AudioContext();
-        analyser = audioCtx.createAnalyser();
-        analyser.fftSize = 64;
-        source = audioCtx.createMediaElementSource(player);
-        source.connect(analyser);
-        source.connect(audioCtx.destination);
-        data = new Uint8Array(analyser.frequencyBinCount);
-        requestAnimationFrame(loopingFunction);
-        analyser.getByteFrequencyData(data);
+  canvas = document.getElementById("mp-visualiser");
+  if (canvas != undefined) {
+    ctx = canvas.getContext("2d");
+    ctx.canvas.width = canvas.width;
+    ctx.canvas.height = canvas.height;
+    audioCtx = new AudioContext();
+    analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 64;
+    source = audioCtx.createMediaElementSource(player);
+    source.connect(analyser);
+    source.connect(audioCtx.destination);
+    data = new Uint8Array(analyser.frequencyBinCount);
+    requestAnimationFrame(loopingFunction);
+    analyser.getByteFrequencyData(data);
 
+    window.addEventListener("resize", resizeCanvas, false);
+  }
 
-        window.addEventListener('resize', resizeCanvas, false);
-    }
-
-    populateTracklist();
-    changeTrack(0);
+  populateTracklist();
+  changeTrack(0);
 });
 
 function loopingFunction() {
-    requestAnimationFrame(loopingFunction);
-    analyser.getByteFrequencyData(data);
-    draw(data);
+  requestAnimationFrame(loopingFunction);
+  analyser.getByteFrequencyData(data);
+  draw(data);
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let space = canvas.width / data.length;
-    var x = 0;
-    var bufferLength = analyser.frequencyBinCount;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  let space = canvas.width / data.length;
+  var x = 0;
+  var bufferLength = analyser.frequencyBinCount;
 
-    var barWidth = (canvas.width / bufferLength) * 2;
-    for (var i = 0; i < bufferLength; i++) {
-        var barHeight = data[i] / 2;
-        //ctx.strokeStyle = "#ffffff";
-        //ctx.beginPath();
-        //ctx.moveTo(space * i, canvas.height); //x,y
-        //ctx.lineTo(space * i, canvas.height - value); //x,y
-        //ctx.stroke();
+  var barWidth = (canvas.width / bufferLength) * 2;
+  for (var i = 0; i < bufferLength; i++) {
+    var barHeight = data[i] / 2;
+    //ctx.strokeStyle = "#ffffff";
+    //ctx.beginPath();
+    //ctx.moveTo(space * i, canvas.height); //x,y
+    //ctx.lineTo(space * i, canvas.height - value); //x,y
+    //ctx.stroke();
 
-        ctx.fillStyle = "white";
-        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-        x += barWidth;
-    }
+    ctx.fillStyle = "white";
+    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+    x += barWidth;
+  }
 }
 
 function resizeCanvas() {
-    ctx.canvas.width = canvas.clientWidth;
-    ctx.canvas.height = canvas.clientHeight;
+  ctx.canvas.width = canvas.clientWidth;
+  ctx.canvas.height = canvas.clientHeight;
 }
