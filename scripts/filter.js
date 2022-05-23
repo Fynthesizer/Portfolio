fe56eaacbee00132ef24811c773d3a0b328a34f4
Fynthesizer <finn.mitchellanyon@gmail.com>
2022-05-23 12:45:57 +1200

    Improved interface for adding portfolio items, added Flutterby to projects list

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,54 +1,12 @@
 /*eslint-env browser*/
 
-filterSelection("all");
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("portfolio-item");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) {
-      w3AddClass(x[i], "show");
-      //$("#portfolio-grid").prepend(x[i]);
-    }
-  }
-}
-
 function getHashFilter() {
-  var hash = location.hash;
-  // get filter=filterName
   var matches = location.hash.match(/filter=([^&]+)/i);
   var hashFilter = matches && matches[1];
   return hashFilter && decodeURIComponent(hashFilter);
 }
 
-var $grid = $("#portfolio-grid");
-
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+let $grid = $("#portfolio-grid");
 
 // Add active class to the current control button (highlight it)
 var $filterButtonGroup = $("#filterContainer");
@@ -63,22 +21,6 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 var isIsotopeInit = false;
-
-window.addEventListener("load", (event) => {
-  $("#portfolio-grid").isotope({
-    percentPosition: true,
-    itemSelector: ".portfolio-item",
-    layoutMode: "fitRows",
-    stagger: 50,
-    transitionDuration: "0.5s",
-    hiddenStyle: {
-      opacity: 0,
-    },
-    visibleStyle: {
-      opacity: 1,
-    },
-  });
-});
 
 function onHashchange() {
   var hashFilter = getHashFilter();
@@ -107,3 +49,26 @@ $(window).on("hashchange", onHashchange);
 
 // trigger event handler to init Isotope
 onHashchange();
+
+document.addEventListener("projectsLoaded", (event) => {
+  const elements = document.getElementsByClassName("portfolio-item");
+  console.log(elements.length);
+  $grid.isotope("appended", elements);
+  let grid = $grid.isotope({
+    percentPosition: true,
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+    stagger: 50,
+    transitionDuration: "0.5s",
+    hiddenStyle: {
+      opacity: 0,
+    },
+    visibleStyle: {
+      opacity: 1,
+    },
+  });
+
+  grid.imagesLoaded().progress(function () {
+    $grid.isotope("layout");
+  });
+});
