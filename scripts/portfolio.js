d7701144483e2284ef0061facdc99dd09fd45d7f
Fynthesizer <finn.mitchellanyon@gmail.com>
2022-07-18 12:56:53 +1200

    made some changes

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -20,48 +20,50 @@ function createPortfolio(projects) {
 
 function createItem(project) {
   //Main div
-  const item = document.createElement("div");
-  item.setAttribute("id", project.id);
-  item.classList.add("portfolio-item");
-  item.classList.add("show");
-  project.categories.forEach((category) => item.classList.add(category));
+  if (project.show) {
+    const item = document.createElement("div");
+    item.setAttribute("id", project.id);
+    item.classList.add("portfolio-item");
+    item.classList.add("show");
+    project.categories.forEach((category) => item.classList.add(category));
 
-  //Anchor
-  const anchor = document.createElement("a");
-  anchor.setAttribute("href", `works/${project.id}.html`);
-  item.appendChild(anchor);
+    //Anchor
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", `works/${project.id}.html`);
+    item.appendChild(anchor);
 
-  //Thumbnail
-  const thumbnail = document.createElement("img");
-  thumbnail.setAttribute("src", project.thumbnail);
-  anchor.appendChild(thumbnail);
+    //Thumbnail
+    const thumbnail = document.createElement("img");
+    thumbnail.setAttribute("src", project.thumbnail);
+    anchor.appendChild(thumbnail);
 
-  //Overlay
-  const overlay = document.createElement("div");
-  overlay.classList.add("overlay");
-  anchor.appendChild(overlay);
-  const overlayText = document.createElement("div");
-  overlayText.classList.add("overlay-text");
-  overlay.appendChild(overlayText);
+    //Overlay
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+    anchor.appendChild(overlay);
+    const overlayText = document.createElement("div");
+    overlayText.classList.add("overlay-text");
+    overlay.appendChild(overlayText);
 
-  //Header
-  const header = document.createElement("div");
-  header.classList.add("overlay-header");
-  overlayText.appendChild(header);
-  const title = document.createElement("div");
-  title.classList.add("overlay-title");
-  title.textContent = project.title;
-  header.appendChild(title);
-  const description = document.createElement("div");
-  description.classList.add("overlay-description");
-  description.textContent = project.description;
-  header.appendChild(description);
+    //Header
+    const header = document.createElement("div");
+    header.classList.add("overlay-header");
+    overlayText.appendChild(header);
+    const title = document.createElement("div");
+    title.classList.add("overlay-title");
+    title.textContent = project.title;
+    header.appendChild(title);
+    const description = document.createElement("div");
+    description.classList.add("overlay-description");
+    description.textContent = project.description;
+    header.appendChild(description);
 
-  //Credits
-  const credits = document.createElement("div");
-  credits.classList.add("overlay-credits");
-  credits.textContent = project.credits;
-  overlayText.appendChild(credits);
+    //Credits
+    const credits = document.createElement("div");
+    credits.classList.add("overlay-credits");
+    credits.textContent = project.credits;
+    overlayText.appendChild(credits);
 
-  portfolioGrid.appendChild(item);
+    portfolioGrid.appendChild(item);
+  }
 }
